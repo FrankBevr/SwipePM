1f523588ad3a9997f7405c89dfbcdb2675e56e25
FrankBevr <frank_dierolf@web.de>
2023-07-17 00:22:19 +0200

    :zap: (./frontend) add UnoCSS

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,6 @@
 import './style.css'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-    <h1>Vite + TypeScript</h1>
+    <h1 class="text-rose-500">Vite + TypeScript</h1>
   </div>
 `
