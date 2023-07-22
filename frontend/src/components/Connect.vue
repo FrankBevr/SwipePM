b708d88afa8f596e568c0c08f5de7687c42ae29a
FrankBevr <frank_dierolf@web.de>
2023-07-22 09:48:16 +0200

    :zap: (./frontend) add connect Button

---
+++ frontend/src/components/Connect.vue
@@ -0,0 +1,15 @@
+<script setup>
+const logMe = () => {
+  console.log("Log Me");
+};
+</script>
+<template>
+  <div class="flex flex-justify-center mb-10 mt-10">
+    <button
+      @click="logMe"
+      class="bg-#CCCCCC font-bold text-2xl p-5 b-rd-3 hover:bg-#ffffff"
+    >
+      Connect
+    </button>
+  </div>
+</template>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,13 +3,16 @@ import "virtual:uno.css";
 import { createApp } from "vue";
 import Header from "./components/Header.vue";
 import Duel from "./components/Duel.vue";
+import Connect from "./components/Connect.vue";
 
 const app = createApp({
   components: {
     Header,
     Duel,
+    Connect,
   },
   template: `
+    <Connect/>
     <Header/>
     <Duel/>
   `,
