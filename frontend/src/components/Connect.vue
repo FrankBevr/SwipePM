1993b3326b0ea4b80a00768e29814b7cf250ae91
FrankBevr <frank_dierolf@web.de>
2023-07-22 09:57:07 +0200

    :zap: (./frontend) add @polkadot/extension-dapp & make it click

--- frontend/src/components/Connect.vue
+++ frontend/src/components/Connect.vue
@@ -1,15 +1,23 @@
-<script setup>
+<script setup lang="ts">
+import { web3Accounts, web3Enable } from "@polkadot/extension-dapp";
+import { onMounted } from "vue";
+let allAccounts: any;
+let allInjected: any;
+
+onMounted(async () => {
+  allInjected = await web3Enable("SwipePM");
+  allAccounts = await web3Accounts();
+});
 const logMe = () => {
   console.log("Log Me");
+  console.log(allAccounts);
 };
 </script>
 <template>
   <div class="flex flex-justify-center mb-10 mt-10">
-    <button
-      @click="logMe"
-      class="bg-#CCCCCC font-bold text-2xl p-5 b-rd-3 hover:bg-#ffffff"
-    >
+    <button @click="logMe" class="bg-#CCCCCC font-bold text-2xl p-5 b-rd-3 hover:bg-#ffffff">
       Connect
     </button>
+    <pre></pre>
   </div>
 </template>
