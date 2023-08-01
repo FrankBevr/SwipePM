1683126746e6262afe84a8e95fb1c9662ae84fa4
FrankBevr <frank_dierolf@web.de>
2023-08-01 21:09:33 +0200

    :zap: (./frontend/src/Components) clean up Connect

--- frontend/src/components/Connect.vue
+++ frontend/src/components/Connect.vue
@@ -1,27 +1,7 @@
-<script setup lang="ts">
-import { web3Accounts, web3Enable } from "@polkadot/extension-dapp";
-import { onMounted } from "vue";
-import { store } from "../store/store";
-import { WsProvider, ApiPromise } from "@polkadot/api";
-
-onMounted(async () => {
-  const wsProvider = new WsProvider();
-  const api = await ApiPromise.create({ provider: wsProvider });
-  store.api = api
-  //@ts-ignore
-  const allInjected = await web3Enable("SwipePM");
-  const allAccounts = await web3Accounts();
-  store.accounts = allAccounts;
-  store.selectedAccount = allAccounts[0];
-});
-const logMe = () => {
-  console.log("Log Me");
-  console.log(`Thats my selected Account: ${store.selectedAccount?.address}`);
-};
-</script>
+<script setup lang="ts"></script>
 <template>
   <div class="flex flex-justify-center mb-10 mt-10">
-    <button @click="logMe" class="bg-#CCCCCC font-bold text-2xl p-5 b-rd-3 hover:bg-#ffffff">
+    <button class="bg-#CCCCCC font-bold text-2xl p-5 b-rd-3 hover:bg-#ffffff">
       Connect
     </button>
     <pre></pre>
