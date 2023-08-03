7dc275cb9d68cbbe6555d9868d3868700eb9bddd
FrankBevr <frank_dierolf@web.de>
2023-08-03 06:07:19 +0200

    :zap: (./frontend) reintroduce @polkadot/extension-dapp
    add get_accounts
    add select_account
    add send_transaction
    add show allAccounts section for quick overiew alias debugging
    add show select selected account for quick overiew alias debugging
    add show metadata for an select account for sake of completion

--- frontend/src/components/PlaygroundCallButtons.vue
+++ frontend/src/components/PlaygroundCallButtons.vue
@@ -13,25 +13,41 @@ import type {
   InjectedExtension,
   InjectedAccountWithMeta,
 } from "@polkadot/extension-inject/types";
-import { ApiPromise, WsProvider } from "@polkadot/api";
 import { Ref, ref } from "vue";
+import { ApiPromise, WsProvider } from "@polkadot/api";
 
 const allInjected: Ref<null | InjectedExtension[]> = ref(null);
 const allAccounts: Ref<null | InjectedAccountWithMeta[]> = ref(null);
-async function accounts() {
-  const wsProvider = new WsProvider();
-  const api = await ApiPromise.create({ provider: wsProvider });
+const selectedAccount: Ref<null | InjectedAccountWithMeta> = ref(null);
+const injector: Ref<null | InjectedExtension> = ref(null);
+const successTransaction: Ref<boolean> = ref(false);
+
+async function get_accounts() {
   allInjected.value = await web3Enable("my cool dapp");
   allAccounts.value = await web3Accounts();
-  const SENDER = "5DTestUPts3kjeXSTMyerHihn1uwMfLj8vU8sqF7qYrFabHE";
-  const injector = await web3FromAddress(SENDER);
+}
+async function select_account(index: number) {
+  allInjected.value = await web3Enable("my cool dapp");
+  allAccounts.value = await web3Accounts();
+  selectedAccount.value = allAccounts.value[index];
+}
+async function send_transaction() {
+  allInjected.value = await web3Enable("my cool dapp");
+  allAccounts.value = await web3Accounts();
+  selectedAccount.value = allAccounts.value[3];
+
+  const wsProvider = new WsProvider();
+  const api = await ApiPromise.create({ provider: wsProvider });
+
+  const SENDER = selectedAccount.value.address;
+  injector.value = await web3FromAddress(SENDER);
+
   api.tx.balances
     .transfer("5C5555yEXUcmEJ5kkcCMvdZjUo7NGJiQJMS7vZXEeoMhj3VQ", 123456)
-    .signAndSend(SENDER, { signer: injector.signer }, (status) => {
-      console.log(status.isInBlock);
+    .signAndSend(SENDER, { signer: injector.value.signer }, (status) => {
+      successTransaction.value = status.isInBlock;
     });
 }
-accounts();
 </script>
 
 <template>
@@ -43,18 +59,51 @@ accounts();
   <SetParticipantChelseaButton></SetParticipantChelseaButton>
   <SetParticpantManchesterButton></SetParticpantManchesterButton>
   <div
-    class="flex flex-justify-center p-10 flex-col text-center outline items-center max-w-xl m-auto backdrop-blur b-rd-3"
-  >
-    <h1>Accounts</h1>
-    <pre class="font-light text-sm text-left w-full">
+    class="flex flex-justify-center p-10 flex-col text-center outline outline-1 items-center max-w-xl m-auto backdrop-blur b-rd-3 border-none">
+    <p class="leading-relaxed">
+      <strong>get_accounts</strong> gets all the accounts which are injected
+    </p>
+    <button
+      class="bg-#CCCCCC hover:bg-#DDDDDD active:bg-#FFFFFF font-light text-base p-5 b-rd-3 max-w-md border-none outline outline-1"
+      @click="get_accounts">
+      get_accounts
+    </button>
+
+    <p class="leading-relaxed">
+      <strong>select_account</strong> gets all the accounts and sets the account
+      based on the chosen index.
+    </p>
+    <button
+      class="bg-#CCCCCC hover:bg-#DDDDDD active:bg-#FFFFFF font-light text-base p-5 b-rd-3 max-w-md border-none outline outline-1"
+      @click="select_account(3)">
+      select_account(3)
+    </button>
+    <p class="leading-relaxed">
+      <strong>send_transaction()</strong> sends transaction to address <br />
+      gets all the accounts which are injected. <br />
+      selects third account which is injected. <br />
+      sets up api <br />
+      send Transaction to 5C5555yEXUcmEJ5kkcCMvdZjUo7NGJiQJMS7vZXEeoMhj3VQ
+    </p>
+    <button
+      class="bg-#CCCCCC hover:bg-#DDDDDD active:bg-#FFFFFF font-light text-base p-5 b-rd-3 max-w-md border-none outline outline-1"
+      @click="send_transaction">
+      send_transaction
+    </button>
+    <p v-if="successTransaction">Its was successfull</p>
+  </div>
+  <div
+    class="flex flex-justify-center p-10 flex-col text-center outline outline-1 items-center max-w-xl m-auto backdrop-blur b-rd-3 border-none">
+    <p class="leading-relaxed">show all Accounts</p>
+    <pre class="font-light text-10px text-left w-full">
       {{ allAccounts }}
     </pre>
-    <h1>Selected Account</h1>
-    <pre class="font-light text-sm text-left w-full" v-if="allAccounts">
+    <p class="leading-relaxed">show selected Accounts</p>
+    <pre class="font-light text-10px text-left w-full" v-if="allAccounts">
      {{ allAccounts[0] }}
     </pre>
-    <h1>Metadata</h1>
-    <pre class="font-light text-sm text-left w-full" v-if="allAccounts">
+    <p class="leading-relaxed">shows metadata of an selected Accounts</p>
+    <pre class="font-light text-10px text-left w-full" v-if="allAccounts">
       {{ allAccounts[0].meta }}
     </pre>
   </div>
