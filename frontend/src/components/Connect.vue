cc1ac2b6b514ae019023d923d07ff44b3d925a92
FrankBevr <frank_dierolf@web.de>
2023-08-05 12:03:05 +0200

    :zap: (./frontend) readjust Connect

--- frontend/src/components/Connect.vue
+++ frontend/src/components/Connect.vue
@@ -15,11 +15,28 @@ async function connect_account(index: number) {
 }
 </script>
 <template>
-  <div class="flex flex-justify-center mb-10 mt-10">
+  <div
+    class="flex flex-justify-center p-10 flex-col text-center outline items-center max-w-xl m-auto backdrop-blur b-rd-3 border-none outline outline-1 text-base font-light mt-10">
+    <p class="leading-relaxed">
+      <strong>Connect</strong> and become the admin of the game.
+      <br />
+      You, as the admin, has the important role to set the winner of the game.
+      <br />
+    <div>For Demo Sake:
+      <ul class="text-left">
+        <li>Alice will pick Manchester </li>
+        <li>Bob will pick Chelsea.</li>
+      </ul>
+    </div>
+    It would be too annoying to switch accounts.
+    <br />
+    You will see what it means in a second.
+    <br />
+    <div><strong>Now connect</strong>, mister Admin.</div>
+    </p>
     <button @click="connect_account(3)"
       class="bg-#CCCCCC text-2xl p-5 b-rd-3 hover:bg-#ffffff outline outline-1 font-light border-none">
       Connect
     </button>
-    <pre></pre>
   </div>
 </template>
