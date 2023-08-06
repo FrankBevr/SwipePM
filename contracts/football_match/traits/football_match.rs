cc98c3a2fba6963e148a406b73e4603288dd923d
FrankBevr <frank_dierolf@web.de>
2023-08-06 09:10:13 +0200

    :zap: (./contracts) add Error type in traits

--- contracts/football_match/traits/football_match.rs
+++ contracts/football_match/traits/football_match.rs
@@ -1,14 +1,16 @@
+use crate::libs::errors::Error;
+
 #[ink::trait_definition]
 pub trait FootballMatch {
     #[ink(message)]
-    fn get_game(&self) -> Result<u8, ()>;
+    fn get_game(&self) -> Result<(), Error>;
 
     #[ink(message)]
-    fn set_winner(&mut self, number: u8) -> Result<(), ()>;
+    fn set_winner(&mut self, number: u8) -> Result<(), Error>;
 
     #[ink(message)]
-    fn set_particpant_chelsea(&mut self) -> Result<(), ()>;
+    fn set_particpant_chelsea(&mut self) -> Result<(), Error>;
 
     #[ink(message)]
-    fn set_particpant_manchester(&mut self) -> Result<(), ()>;
+    fn set_particpant_manchester(&mut self) -> Result<(), Error>;
 }
