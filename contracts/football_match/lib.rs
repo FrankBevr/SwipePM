bd3c64cdb99dfb5856c067a6c1397e7e91a60967
FrankBevr <frank_dierolf@web.de>
2023-08-06 09:11:01 +0200

    :zap: (./contracts) readjust Contract to new contract trait

--- contracts/football_match/lib.rs
+++ contracts/football_match/lib.rs
@@ -1,16 +1,20 @@
 #![cfg_attr(not(feature = "std"), no_std, no_main)]
+pub mod impls;
+pub mod libs;
 pub mod traits;
 
 #[ink::contract]
-mod football_match {
+pub mod football_match {
+    pub use crate::libs::errors::Error;
     pub use crate::traits::football_match::FootballMatch;
 
     #[ink(storage)]
     pub struct GameData {
-        winner: u8,
-        particpant_chelsea: AccountId,
-        particpant_manchester: AccountId,
+        pub winner: u8,
+        pub particpant_chelsea: AccountId,
+        pub particpant_manchester: AccountId,
     }
+
     impl GameData {
         #[ink(constructor)]
         pub fn new() -> Self {
@@ -24,34 +28,22 @@ mod football_match {
 
     impl FootballMatch for GameData {
         #[ink(message)]
-        fn get_game(&self) -> Result<u8, ()> {
-            ink::env::debug_println!("Thast my winer {:?}", self.winner);
-            ink::env::debug_println!("Thast my chelsea {:?}", self.particpant_chelsea);
-            ink::env::debug_println!("Thast my manchester {:?}", self.particpant_manchester);
-            Ok(self.winner)
+        fn get_game(&self) -> Result<(), Error> {
+            Ok(())
         }
         #[ink(message)]
-        fn set_winner(&mut self, number: u8) -> Result<(), ()> {
+        fn set_winner(&mut self, number: u8) -> Result<(), Error> {
             self.winner = number;
-            ink::env::debug_println!("Now the winner is set. The Winner is {:?}", self.winner);
             Ok(())
         }
         #[ink(message)]
-        fn set_particpant_chelsea(&mut self) -> Result<(), ()> {
+        fn set_particpant_chelsea(&mut self) -> Result<(), Error> {
             self.particpant_chelsea = Self::env().caller();
-            ink::env::debug_println!(
-                "Now the Chelsea is set. The Chelsea is {:?}",
-                self.particpant_chelsea
-            );
             Ok(())
         }
         #[ink(message)]
-        fn set_particpant_manchester(&mut self) -> Result<(), ()> {
+        fn set_particpant_manchester(&mut self) -> Result<(), Error> {
             self.particpant_manchester = Self::env().caller();
-            ink::env::debug_println!(
-                "Now the Manchester is set. The Manchester is {:?}",
-                self.particpant_manchester
-            );
             Ok(())
         }
     }
