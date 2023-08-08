2aff0a638243d2db2c58892a26aef13845e10127
FrankBevr <frank_dierolf@web.de>
2023-08-08 08:05:13 +0200

    :zap: (./contracts) add external docs for rustdoc

---
+++ contracts/football_match/docs/struct_GameData.md
@@ -0,0 +1,29 @@
+The `GameData` struct stores values related to a football_match.
+
+It has the following fields:
+- `winning_team`: an integer representing the winning team.
+- `admin`: an integer representing the current admin.
+- `particpant_chelsea`: an AccountId representing the player, who bets on chelse.
+- `particpant_manchester`: an AccountId representing the player, who bets on manchester.
+- `particpant_chelsea_is_set`: a boolean representing, if particpant_chelsea is already set.
+- `particpant_manchester_is_set`: a boolean representing, if particpant_chelsea is already set.
+
+# Examples
+
+```
+use football_match::football_match::GameData;
+use ink::primitives::AccountId;
+
+let game = GameData {
+    winning_team: 0,
+    admin: AccountId::from([0xFF as u8; 32]),
+    particpant_chelsea: AccountId::from([0xFF as u8; 32]),
+    particpant_manchester: AccountId::from([0xFF as u8; 32]),
+    particpant_chelsea_is_set: false,
+    particpant_manchester_is_set: false,
+};
+
+assert!(game.winning_team == 0u8);
+assert!(game.particpant_chelsea_is_set == false);
+assert!(game.particpant_manchester == AccountId::from([0xFF as u8; 32]))
+```
--- contracts/football_match/lib.rs
+++ contracts/football_match/lib.rs
@@ -1,29 +1,4 @@
-//! # football_match
-//!
-//! `football_match` is a small smart contract.\
-//! football_match contains the logic for [SwipePM Dapp](https://github.com/FrankBevr/SwipePM).\
-//! SwipePM is a betting dapp.\
-//! It allows to bet on Manchester or Chelsea.\
-//!
-//! ## Overview (Userflow & Logic)
-//! 1. **Alice** instantiates the contract.
-//! 2. **Alice** becomes the admin.
-//! 3. **Bob** uses the frontend and bets on Manchester.
-//! 4. **Charlie** uses the frontend and isn't allowed to bet on Manchester.
-//! 5. **Charlie** bets on Chelsea.
-//! 6. The amount of the bet is controlled by the frontend.
-//! 7. **Alice**, the admin, calls `set_winner` with the winner value.\
-//!     If Manchester won the value has to be 1\
-//!     If Chelsea won the value has to be 2\
-//!     The Default value is 0\
-//! 8. **Alice** sends value of 1 via `set_winner`.
-//! 9. Manchester won.
-//! 10. **Bob**, the manchester better, recieves all funds.
-//! 11. **Alice** calls `restart_match`, if a new manchester vs chelsea game is happening.\
-//!      `restart_match` resets all values, expect the current admin.
-//! 12. **Alice** will be busy in the upcomming match.
-//! 13. **Alice** calls `change_admin` and sets **Django** as the new admin.
-//! 14. **Djange** is excited.
+#![doc = include_str!("./docs/lib.md")]
 #[warn(missing_docs)]
 #[cfg_attr(not(feature = "std"), no_std, no_main)]
 
@@ -44,6 +19,7 @@ pub mod football_match {
     /***********
      *   Data  *
      ***********/
+    #[doc = include_str!("./docs/struct_GameData.md")]
     #[ink(storage)]
     pub struct GameData {
         pub winning_team: u8,
@@ -72,6 +48,7 @@ pub mod football_match {
     /**********
      * Events *
      **********/
+    /// Represents an emitted event when a get_game is called.
     #[ink(event)]
     pub struct GameState {
         #[ink(topic)]
